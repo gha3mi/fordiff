2c5fe3c1b18778f3afa698b0e0f75679fa84514e
gha3mi <36126354+gha3mi@users.noreply.github.com>
2025-09-11 21:16:26 +0000

    Deploy FORD docs

--- src/demo3.f90
+++ src/demo3.f90
@@ -17,19 +17,19 @@ program demo3
 
    ! Compute derivative of a function using complex-step
    dfdx = derivative(f=f1, x=[1.0_rk, -1.0_rk], h=tiny(0.0_rk))
-   print'(a,*(g0,", "),a)', 'dfdx = ', dfdx, ' (complex-step)'
+   print'(a,6(g0,", "),a)', 'dfdx = ', dfdx, ' (complex-step)'
 
    ! Compute derivative of a function using forward finite-difference
    dfdx = derivative(f=f2, x=[1.0_rk, -1.0_rk], h=1e-5_rk, method='forward')
-   print'(a,*(g0,", "),a)', 'dfdx = ', dfdx, ' (forward finite-difference)'
+   print'(a,6(g0,", "),a)', 'dfdx = ', dfdx, ' (forward finite-difference)'
 
    ! Compute derivative of a function using backward finite-difference
    dfdx = derivative(f=f2, x=[1.0_rk, -1.0_rk], h=1e-5_rk, method='backward')
-   print'(a,*(g0,", "),a)', 'dfdx = ', dfdx, ' (backward finite-difference)'
+   print'(a,6(g0,", "),a)', 'dfdx = ', dfdx, ' (backward finite-difference)'
 
    ! Compute derivative of a function using central finite-difference
    dfdx = derivative(f=f2, x=[1.0_rk, -1.0_rk], h=1e-5_rk, method='central')
-   print'(a,*(g0,", "),a)', 'dfdx = ', dfdx, ' (central finite-difference)'
+   print'(a,6(g0,", "),a)', 'dfdx = ', dfdx, ' (central finite-difference)'
 
 contains
 
